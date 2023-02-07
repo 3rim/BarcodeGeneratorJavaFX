ca9ce1fdfcd2842901af08ad889ab83e639eb245
3rim <erim_medi@hotmail.de>
2023-02-07 12:30:02 +0100

    bugFix:
    errorMsg: java.lang.IllegalAccessException: module javafx.base cannot
    access class org.example.controller because module does not open
    org.example.controller
    
    The module-info updated:
    	Every package like controller needs to be exported and opend or
    	otherwise javafx does not have access to it
    
    https://stackoverflow.com/questions/67372505/java-lang-illegalaccessexception-module-javafx-base-cannot-access-class-sample

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,13 @@ module org.example {
     requires javafx.fxml;
 
     opens org.example to javafx.fxml;
+    opens org.example.controller to javafx.fxml;
+
     exports org.example;
+    exports org.example.controller;
+
+    /**
+     * https://stackoverflow.com/questions/67372505/java-lang-illegalaccessexception-module-javafx-base-cannot-access-class-sample
+     * Die Ordner mussen scheinbar exportiert werden, sonst kann javafx nicht auf die Ordner wie Controller etc. zugreifen
+     */
 }
