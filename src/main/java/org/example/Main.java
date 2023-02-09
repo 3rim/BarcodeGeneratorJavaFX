e22d5d34f19784f2f75ba9ccee4d17114e40be9e
3rim <erim_medi@hotmail.de>
2023-02-09 21:18:20 +0100

    Build executable jar

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * https://www.youtube.com/watch?v=EyYb0GmtEX4
+ * Jar files dont like having running a class that extending something else
+ */
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
