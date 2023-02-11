78c13da9339bfb220e5a2f23cfea180f81b9a1df
3rim <erim_medi@hotmail.de>
2023-02-11 21:44:55 +0100

    added: removeData

--- src/main/java/org/example/controller/InputViewController.java
+++ src/main/java/org/example/controller/InputViewController.java
@@ -7,24 +7,34 @@ import com.itextpdf.text.pdf.Barcode128;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.example.App;
 import org.example.model.ITableLayout;
-import org.example.model.Layout1;
 import org.example.model.TableFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class  InputViewController {
+public class  InputViewController implements Initializable {
     private static String FILE ="src/main/resources/FirstPdf.pdf";
 
     @FXML
@@ -38,7 +48,17 @@ public class  InputViewController {
     //TODO a: If tableLayout is not static tableLayout becomes null, why?
     private static ITableLayout tableLayout;
     private boolean toBarcode = false;
+    private ObservableList<String> items;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        listView.setEditable(true);
+        listView.setCellFactory(TextFieldListCell.forListView());
+
+        /*items = FXCollections.observableArrayList();
+        listView.setItems(items);*/
+    }
 
     @FXML
     private void switchToStartView() throws IOException {
@@ -51,12 +71,26 @@ public class  InputViewController {
         {
             listView.getItems().add(inputField.getText());
             inputField.clear();
+            /*items.add(inputField.getText());
+            inputField.clear();*/
+
         }
     }
 
     @FXML
     private void onEnter(ActionEvent event){
         addButton();
+    }
+
+    @FXML
+    private void removeData(ActionEvent event) {
+        ObservableList<Integer> indices = listView.getSelectionModel().getSelectedIndices().sorted();
+
+        for (int k = indices.size() - 1; k >= 0; k--)
+        {
+            listView.getItems().remove((int) indices.get(k));
+        }
+        listView.getSelectionModel().clearSelection();
     }
 
     @FXML
@@ -123,4 +157,6 @@ public class  InputViewController {
     public void test(){
         System.out.println("startViewController bound to inputViewController ");
     }
+
+
 }
