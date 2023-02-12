f2c712789b3c7b15ae87c24fcd61bb5c55ea6ead
3rim <erim_medi@hotmail.de>
2023-02-12 16:53:57 +0100

    Commentary for PDF pixel <--> Points

--- src/main/java/org/example/model/ITableLayout.java
+++ src/main/java/org/example/model/ITableLayout.java
@@ -3,6 +3,18 @@ package org.example.model;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfPTable;
 
+/**
+ * The size of PDF pages is not expressed in pixels but in points.
+ *
+ * 1 inch = 72 points
+ * 1 inch = 25.4 mm
+ * That leads to:
+ *
+ * 1 point = 0.352777778 mm
+ *
+ * Height of a cell = 30 mm = 85.35826771653 point (printer’s)
+ * Width of a cell = 52 mm = 147,9543307087 point (printer’s)
+ */
 public interface ITableLayout {
 
     PdfPTable getTable();
